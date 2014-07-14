3f7092820a2452f9ee79e6fb9556304652dda124
miclf <michael@estsurinter.net>
2014-07-14 22:32:37 +0200

    Add DocumentProvider class
    This serves as an intermediary layer between scrapers and the URL repository and provides documents according to pattern identifiers.
    
    The provider relies on a configurable implementation of UrlRepositoryInterface. It defaults on instantiating a JsonUrlRepository if none is provided.
    It downloads documents via Guzzle.

---
+++ src/DocumentProvider.php
@@ -0,0 +1,130 @@
+<?php namespace Pandemonium\Methuselah;
+
+use Pandemonium\Methuselah\UrlRepositories\UrlRepositoryInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Query;
+
+/**
+ * Download documents and provide their content.
+ *
+ * @author Michaël Lecerf <michael@estsurinter.net>
+ */
+class DocumentProvider
+{
+    /**
+     * A URL repository.
+     *
+     * @var \Pandemonium\Methuselah\UrlRepositories\UrlRepositoryInterface
+     */
+    protected $urlRepository;
+
+    /**
+     * Specify how query string values are URL encoded.
+     *
+     * @var string|false
+     */
+    protected $queryEncoding;
+
+    /**
+     * Constructor.
+     *
+     * @param array  $config
+     * @return void
+     */
+    public function __construct(array $config = [])
+    {
+        $this->configureUrlRepository($config);
+
+        // Allow to specify how query string values are URL encoded.
+        // This is necessary for bogus sites that do not comply with RFCs.
+        if (array_key_exists('query_encoding', $config)) {
+            $this->queryEncoding = $config['query_encoding'];
+        }
+    }
+
+    /**
+     * Get a web page for a given key.
+     *
+     * @param  string  $key  A URL pattern identifier
+     * @return string        Source code of the page
+     */
+    public function get($key)
+    {
+        $url = $this->urlRepository->find($key);
+
+        return $this->download($url);
+    }
+
+    /**
+     * Get the URL repository of the document provider.
+     *
+     * @return \Pandemonium\Methuselah\UrlRepositories\UrlRepositoryInterface
+     */
+    public function getUrlRepository()
+    {
+        return $this->urlRepository;
+    }
+
+    /**
+     * Create a new HTTP client.
+     *
+     * @return \GuzzleHttp\Client
+     */
+    public function newHttpClient()
+    {
+        return new Client;
+    }
+
+    /**
+     * Download the content at the given URL.
+     *
+     * @param  string  $url
+     * @return string
+     */
+    protected function download($url)
+    {
+        $client = $this->newHttpClient();
+
+        $request = $client->createRequest('GET', $url);
+
+        // Some sites, such as the one of the Chamber, do not respect RFC
+        // 1738 nor RFC 3986. In order to work with them, query strings
+        // must not be encoded. We then need to provide a way to disable
+        // the automatic encoding that Guzzle provides by default.
+        if (isset($this->queryEncoding)) {
+            $request->getQuery()->setEncodingType($this->queryEncoding);
+        }
+
+        // Return the body of the response.
+        return (string) $client->send($request)->getBody();
+    }
+
+    /**
+     * Set the URL repository according to the given config.
+     *
+     * @param  array  $config
+     * @return void
+     */
+    protected function configureUrlRepository($config)
+    {
+        if (array_key_exists('url_repository', $config)) {
+            $repo = $config['url_repository'];
+        }
+
+        if (isset($repo) && $repo instanceof UrlRepositoryInterface) {
+            $this->urlRepository = $repo;
+        } else {
+            $this->setDefaultUrlRepository();
+        }
+    }
+
+    /**
+     * Set up the URL repository with the default class.
+     *
+     * @return void
+     */
+    protected function setDefaultUrlRepository()
+    {
+        $this->urlRepository = new UrlRepositories\JsonUrlRepository;
+    }
+}
