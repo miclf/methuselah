0c7e1be6c0ffb4074d33810eb0f5484335957dd9
miclf <michael@estsurinter.net>
2014-10-16 08:24:46 +0200

    Add S crawler to get the list of committees

---
+++ src/Scrapers/S/CommitteeList.php
@@ -0,0 +1,183 @@
+<?php namespace Pandemonium\Methuselah\Scrapers\S;
+
+use Exception;
+use Pandemonium\Methuselah\Crawler\Crawler;
+use Pandemonium\Methuselah\Scrapers\AbstractScraper;
+
+/**
+ * Extract data from the list of committees of the Senate.
+ *
+ * @author Michaël Lecerf <michael@estsurinter.net>
+ */
+class CommitteeList extends AbstractScraper
+{
+    /**
+     * Character set of the scraped documents.
+     *
+     * @var string
+     */
+    protected $charset = 'ISO-8859-1';
+
+    /**
+     * The list of committee types, mapped
+     * to their French and Dutch names.
+     *
+     * @var array
+     */
+    protected $types = [
+        // French
+        "commission mixte"      => 'mixed',
+        "commission spéciale"   => 'special',
+        "commission permanente" => 'permanent',
+
+        // Dutch
+        "gemengde commissie"   => 'mixed',
+        "bijzondere commissie" => 'special',
+        "vaste commissie"      => 'permanent',
+    ];
+
+    /**
+     * The list of committees.
+     *
+     * @var array
+     */
+    protected $committees = [];
+
+    /**
+     * Scrape the pages of lists of committees
+     * and extract their information.
+     *
+     * @return array
+     *
+     * @throws \Exception if a committee type cannot be determined.
+     */
+    public function scrape()
+    {
+        foreach (['fr', 'nl'] as $lang) {
+            $this->scrapeCommitteeList($lang);
+        }
+
+        return $this->getCommittees();
+    }
+
+    /**
+     * Return the appropriate parameters for the document provider.
+     *
+     * This returns an indexed array of two elements. The first is the
+     * pattern string and the second is an array of pattern values.
+     *
+     * @param  string  $lang
+     * @return array
+     */
+    protected function getProviderArguments($lang = 'fr')
+    {
+        return ['s.committee_list', ['lang' => $lang]];
+    }
+
+    /**
+     * Scrape a page of list of committees
+     * and extract its information.
+     *
+     * @param  string  $lang
+     * @return array
+     *
+     * @throws \Exception if a committee type cannot be determined.
+     */
+    protected function scrapeCommitteeList($lang)
+    {
+        $type = null;
+
+        $this->getNodes($lang)->each(function ($node) use (&$type, $lang) {
+
+            // If the current node introduces a committee type, we
+            // store it and, until we encounter a new one, all the
+            // next committees will be of that type.
+            if ($str = $this->getCommitteeType($node)) {
+
+                $type = $str;
+
+                return;
+            }
+
+            // Get the identifier of the current committee.
+            $matches = $this->match('#COMID=(\d+)#', $node->attr('href'));
+            $identifier = $matches[1];
+
+            // Determine the committee name in the current language.
+            $name = $node->text();
+
+            // If this committee has already been added to the list in
+            // a previous iteration, then we only need to add the name
+            // in the current lang, because the rest of the data is
+            // already there. Otherwise, we set everything.
+            if (isset($this->committees[$identifier])) {
+
+                $this->committees[$identifier]['name_'.$lang] = $name;
+
+            } else {
+
+                $this->committees[$identifier] = [
+                    'identifier'  => $identifier,
+                    'type'        => $type,
+                    'name_'.$lang => $name,
+                ];
+            }
+        });
+    }
+
+    /**
+     * Get a DOM crawler prefilled with only the
+     * node set relevant for the scraper.
+     *
+     * @return \Pandemonium\Methuselah\Crawler\Crawler
+     */
+    public function getNodes($lang)
+    {
+        list($pattern, $values) = $this->getProviderArguments($lang);
+
+        $document = $this->getDocument($pattern, $values);
+
+        return $this->getCrawler($document)->filter('h2, li a');
+    }
+
+    /**
+     * Get a committee type from a node.
+     *
+     * @param  \Symfony\Component\DomCrawler\Crawler  $node
+     * @return string|null
+     *
+     * @throws \Exception if the type cannot be determined.
+     */
+    protected function getCommitteeType(Crawler $node)
+    {
+        if ($node->getNode(0)->tagName === 'h2') {
+
+            // The tag contains the French or Dutch name of the type. We
+            // loop on a map to find the associated ‘normalized’ name.
+            $str = trim($node->text());
+
+            foreach ($this->types as $needle => $type) {
+                if ($str === $needle) return $type;
+            }
+
+            throw new Exception('Cannot determine type of committee');
+        }
+    }
+
+    /**
+     * Return the list of committees as an indexed
+     * array of associative arrays.
+     *
+     * @return array
+     */
+    protected function getCommittees()
+    {
+        $list = [];
+
+        foreach ($this->committees as $committee) {
+            $list[] = $committee;
+        }
+
+        return $list;
+    }
+}
