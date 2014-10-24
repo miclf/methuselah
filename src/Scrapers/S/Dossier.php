31454987ca2e92952712b52c0e22c7e30e2eb5fe
miclf <michael@estsurinter.net>
2014-10-24 17:30:56 +0200

    Add empty scraper for the dossiers of the Senate
    This mainly sets up the getProviderArguments() implementation.

---
+++ src/Scrapers/S/Dossier.php
@@ -0,0 +1,56 @@
+<?php namespace Pandemonium\Methuselah\Scrapers\S;
+
+use Pandemonium\Methuselah\Crawler\Crawler;
+
+/**
+ * Extract data from the pages of dossiers of the Senate.
+ *
+ * @author Michaël Lecerf <michael@estsurinter.net>
+ */
+class Dossier extends AbstractScraper
+{
+    /**
+     * An instance of a DOM crawler.
+     *
+     * @var \Pandemonium\Methuselah\Crawler\Crawler
+     */
+    protected $crawler;
+
+    /**
+     * Scrape the page of a dossier and extract its information.
+     *
+     * @return array
+     */
+    public function scrape()
+    {
+        $this->crawler = $this->getCrawler();
+
+        $dossier = [];
+
+        return $dossier;
+    }
+
+    /**
+     * Return the appropriate parameters for the document provider.
+     *
+     * This returns an indexed array of two elements. The first is the
+     * pattern string and the second is an array of pattern values.
+     *
+     * @param  string  $lang
+     * @return array
+     */
+    protected function getProviderArguments($lang = 'fr')
+    {
+        // Split the dossier identifier to get the legislature
+        // number and the number of the dossier.
+        $matches = $this->match('#(\d+)S(\d+)#', $this->getOption('identifier'));
+
+        $values = [
+            'legislatureNumber' => $matches[1],
+            'dossierNumber'     => $matches[2],
+            'lang'              => $this->getOption('lang', 'fr'),
+        ];
+
+        return ['s.dossier', $values];
+    }
+}
