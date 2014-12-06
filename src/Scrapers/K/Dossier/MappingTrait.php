3aa1a2394c37800d302f165026accf7a76a9787e
miclf <michael@estsurinter.net>
2014-12-07 00:46:45 +0100

    Remove root node names from mapping destinations

--- src/Scrapers/K/Dossier/MappingTrait.php
+++ src/Scrapers/K/Dossier/MappingTrait.php
@@ -25,41 +25,41 @@ trait MappingTrait
         // These nodes will be grouped as metadata.
         'meta' => [
             // Number of the dossier in K format (00K0000)
-            'n-du-document'   => 'meta.number',
+            'n-du-document'   => 'number',
             // Bicameral number of the dossier
-            'chambre-etou-senat.document-principal.0.n-bicam' => 'meta.bicameralNumber',
+            'chambre-etou-senat.document-principal.0.n-bicam' => 'bicameralNumber',
             // Number of the legislature
-            'legislature'     => 'meta.legislature',
+            'legislature'     => 'legislature',
             // Type of dossier
             'chambre-etou-senat.document-principal.0.type.code' => [
-                'destination' => 'meta.dossierType',
+                'destination' => 'dossierType',
                 'dictionary'  => 'dossierTypes',
             ],
             // Current status
             'etat-davancement.chambre-fr' => [
-                'destination' => 'meta.status.chamber',
+                'destination' => 'status.chamber',
                 'dictionary'  => 'dossierStatuses',
             ],
             // Relevant article of the constitution, if any
-            'article-constitution.code' => 'meta.constitutionalArticle',
+            'article-constitution.code' => 'constitutionalArticle',
             // Date of submission
             'chambre-etou-senat.document-principal.0.date-de-depot' => [
-                'destination' => 'meta.dates.submission',
+                'destination' => 'dates.submission',
                 'modifier'    => 'dateToIso'
             ],
             // Date of consideration
             'chambre-etou-senat.document-principal.0.prise-en-consideration' => [
-                'destination' => 'meta.dates.consideration',
+                'destination' => 'dates.consideration',
                 'modifier'    => 'dateToIso'
             ],
             // Date of distribution among the MPs
             'chambre-etou-senat.document-principal.0.date-de-distribution' => [
-                'destination' => 'meta.dates.distribution',
+                'destination' => 'dates.distribution',
                 'modifier'    => 'dateToIso'
             ],
             // Sending date
             'chambre-etou-senat.document-principal.0.date-denvoi' => [
-                'destination' => 'meta.dates.sending',
+                'destination' => 'dates.sending',
                 'modifier'    => 'dateToIso'
             ],
         ],
