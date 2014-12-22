eaa2c7886f38583d11908aaeca8ec1730a4fa24a
miclf <michael@estsurinter.net>
2014-12-22 01:11:23 +0100

    Add two new types of committees

--- src/Scrapers/K/CommitteeList.php
+++ src/Scrapers/K/CommitteeList.php
@@ -21,12 +21,16 @@ class CommitteeList extends AbstractScraper
         // French
         "SPÉCIALES"               => 'special',
         "COMITÉS D'AVIS"          => 'advisory',
+        "GROUPES DE TRAVAIL"      => 'working-group',
         "COMMISSIONS PERMANENTES" => 'permanent',
+        "DÉLÉGATIONS INTERNATIONALES" => 'international-delegation',
 
         // Dutch
         "BIJZONDERE"              => 'special',
         "ADVIESCOMITÉS"           => 'advisory',
+        "WERKGROEPEN"             => 'working-group',
         "VASTE COMMISSIES"        => 'permanent',
+        "INTERNATIONALE AFGEVAARDIGDEN" => 'international-delegation',
     ];
 
     /**
