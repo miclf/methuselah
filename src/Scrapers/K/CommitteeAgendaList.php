6c9751ad9c420b32317dccf65bc035fb1999639b
miclf <michael@estsurinter.net>
2015-01-16 23:01:35 +0100

    Add main ‘list’ scraper for K committee agendas
    This scraper targets the main page of the committee agenda of the Chamber.
    
    Depending of the content of this page, the scraper instantiates and calls other scrapers in order to get a list of URLs targeting detailed committee agendas.
    
    In other words, this scrapers acts as an ‘intelligent’ intermediary, putting itself in front of more specialized scrapers and choosing which one(s) is/are the more appropriate for the job.

---
+++ src/Scrapers/K/CommitteeAgendaList.php
@@ -0,0 +1,212 @@
+<?php namespace Pandemonium\Methuselah\Scrapers\K;
+
+use Illuminate\Container\Container;
+use Pandemonium\Methuselah\Crawler\Crawler;
+
+/**
+ * Extract links to current agenda pages
+ * of detailed committees meetings.
+ *
+ * Each of the returned URLs targets a page detailing
+ * the meetings of a given committee (or group of
+ * committees) that are planned for the week.
+ *
+ * @author Michaël Lecerf <michael@estsurinter.net>
+ */
+class CommitteeAgendaList extends AbstractScraper
+{
+    /**
+     * An instance of a DOM crawler.
+     *
+     * @var \Pandemonium\Methuselah\Crawler\Crawler
+     */
+    protected $crawler;
+
+    /**
+     * Scrape the committee agenda to find links
+     * to pages of committee meetings.
+     *
+     * When we reach the main page of the committee agenda,
+     * we can face three different types of content:
+     *
+     *   1. A list of multiple weeks of meetings
+     *   2. A series of meetings for a given week
+     *   3. A page listing meetings of a specific committee
+     *
+     * This scraper detects the type of content that is on the page and
+     * acts accordingly. The returned result is always supposed to be
+     * a list of URLs, each one targeting a page listing meetings
+     * for a specific committee (or group of committees).
+     *
+     * @return array
+     */
+    public function scrape()
+    {
+        // Store a crawler for the main page of committee agenda.
+        // We will reuse it later in a way that will depend on
+        // the type of content it stores.
+        $this->crawler = $this->getCrawler();
+
+        $commAgendaUrls = [];
+
+        // Case 3: a page listing meetings of a specific committee.
+        if ($this->isSingleCommitteeAgenda()) {
+            // Not implemented yet.
+        }
+
+        if ($this->isListOfWeeks()) {
+            // Case 1: a list of multiple weeks of meetings.
+            $commAgendaUrls = $this->getAgendasForMultipleWeeks();
+        } else {
+            // Case 2: a series of meetings for a given week.
+            $commAgendaUrls = $this->getAgendasForSingleWeek();
+        }
+
+        return $commAgendaUrls;
+    }
+
+    /**
+     * Check if the current page lists meetings
+     * of a specific committee.
+     *
+     * @return bool
+     */
+    protected function isSingleCommitteeAgenda()
+    {
+        // TODO: implement this.
+        return false;
+    }
+
+    /**
+     * Check if the current page contains a
+     * list of multiple weeks of meetings.
+     *
+     * @return bool
+     */
+    protected function isListOfWeeks()
+    {
+        $selector = "h3:contains('TABLEAU DES REUNIONS DE COMMISSION')";
+
+        $span = $this->crawler->filter($selector);
+
+        return (bool) count($span);
+    }
+
+    /**
+     * From a list of multiple weeks of meetings, get a list
+     * of URLs of meeting pages of specific committees.
+     *
+     * @return array
+     */
+    protected function getAgendasForMultipleWeeks()
+    {
+        $commAgendaUrls = [];
+
+        foreach ($this->getWeekUrls() as $weekUrl) {
+            $urls = $this->getCommitteeAgendaUrls($weekUrl);
+            $commAgendaUrls = array_merge($urls, $commAgendaUrls);
+        }
+
+        return $this->sort($commAgendaUrls);
+    }
+
+    /**
+     * Scrape an array of URLs from a list
+     * of multiple weeks of meetings.
+     *
+     * @return array
+     */
+    protected function getWeekUrls()
+    {
+        $scraper = $this->makeScraper('CommitteeMeetingWeekList');
+
+        // We already have a crawler storing the content
+        // to scrape, so we inject it to avoid uselessly
+        // downloading this content one more time.
+        $scraper->setOptions(['crawler' => $this->crawler]);
+
+        $data = $scraper->scrape();
+
+        // The scraper gives us a bunch of data about each week. We extract
+        // the URLs because that’s the only info we’re interested in.
+        $weekUrls = array_pluck($data, 'url');
+
+        return $this->sort($weekUrls);
+    }
+
+    /**
+     * Scrape an array of URLs from a given page listing
+     * a series of meetings for a given week.
+     *
+     * @param  string  $weekUrl
+     * @return array
+     */
+    protected function getCommitteeAgendaUrls($weekUrl)
+    {
+        $scraper = $this->makeScraper('CommitteeMeetingList');
+
+        $scraper->setOptions(['url' => $weekUrl]);
+
+        return $scraper->scrape();
+    }
+
+    /**
+     * Scrape an array of URLs from a given DOM crawler
+     * listing a series of meetings for a given week.
+     *
+     * @return array
+     */
+    protected function getAgendasForSingleWeek()
+    {
+        $scraper = $this->makeScraper('CommitteeMeetingList');
+
+        // Since this class’ DOM crawler already stores the content
+        // we want to scrape, we will simply pass it, thus avoiding
+        // to uselessly download the page once again.
+        $scraper->setOptions(['crawler' => $this->crawler]);
+
+        $urls = $scraper->scrape();
+
+        return $this->sort($urls);
+    }
+
+    /**
+     * Instantiate an agenda scraper of the given class.
+     *
+     * @param  string  $class
+     * @return \Pandemonium\Methuselah\Scrapers\ScraperInterface
+     */
+    protected function makeScraper($class)
+    {
+        $fullClassName = __NAMESPACE__.'\\'.$class;
+
+        return (new Container)->make($fullClassName);
+    }
+
+    /**
+     * Sort an array by value.
+     *
+     * @param  array  $array
+     * @return array
+     */
+    protected function sort(array $array)
+    {
+        sort($array);
+
+        return $array;
+    }
+
+    /**
+     * Return the appropriate parameters for the document provider.
+     *
+     * This returns an indexed array of two elements. The first is the
+     * pattern string and the second is an array of pattern values.
+     *
+     * @param  string  $lang
+     * @return array
+     */
+    protected function getProviderArguments()
+    {
+        return ['k.agenda_list.committee', []];
+    }
+}
