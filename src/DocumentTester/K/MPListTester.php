f812ca09462af2efc1f9801791f5909155e10420
miclf <michael@estsurinter.net>
2015-02-15 12:30:57 +0100

    Add tester command for K MP list scraper

---
+++ src/DocumentTester/K/MPListTester.php
@@ -0,0 +1,18 @@
+<?php namespace Pandemonium\Methuselah\DocumentTester\K;
+
+use Pandemonium\Methuselah\DocumentTester\ScraperTesterCommand;
+
+/**
+ * Command to test the K\MPList scraper.
+ *
+ * @author Michaël Lecerf <michael@estsurinter.net>
+ */
+class MPListTester extends ScraperTesterCommand
+{
+    /**
+     * Name of the command.
+     *
+     * @var string
+     */
+    protected $name = 'k:mp-list';
+}
